feae93b5fbb76d8cd90b7df07c8731f41bbd6cda
Mainfrezzer <35963833+Mainfrezzer@users.noreply.github.com>
2024-12-21 11:38:00 +0100

    Ipv6 capabilities check
    Added an ipv6 check to determine which unbound config is being used in case non was provided.

--- additions/start.sh
+++ additions/start.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-if [ ! -f /opt/adguardhome/conf/unbound.conf ]; then
- cp /etc/unbound/unbound.conf /opt/adguardhome/conf/unbound.conf
-echo "Custom config not found. Copied a config"
+# Check for IPv6 connectivity
+if ip -6 address show | awk '!/1/ && /inet6/' | grep -q 'inet6'; then
+    #echo "There is an interface with IPv6 connectivity."
+	if [ ! -f /opt/adguardhome/conf/unbound.conf ]; then
+	 cp /etc/unbound/unbound-ip6.conf /opt/adguardhome/conf/unbound.conf
+	 echo "No Custom Unbound.conf found, created ipv6 config"
+	fi
+else
+    #echo "No interface with IPv6 connectivity found."
+	if [ ! -f /opt/adguardhome/conf/unbound.conf ]; then
+	 cp /etc/unbound/unbound.conf /opt/adguardhome/conf/unbound.conf
+	 echo "No Custom Unbound.conf found, created ipv4 config."
+	fi
 fi
 
 sh -c crond
